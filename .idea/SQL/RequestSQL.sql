6c2f67db35f66a30de012ec44ab0ea617a365881
walerikq <walerikq@yandex.ru>
2022-12-05 16:30:02 +0300

    requestTaskComplete

--- .idea/SQL/RequestSQL.sql
+++ .idea/SQL/RequestSQL.sql
@@ -7,13 +7,41 @@ WHERE Firstname LIKE 'А%';
 SELECT firstname,lastname,CITY.name AS city FROM customers,city
 WHERE CITY.id = customers.id AND NAME LIKE 'Н%';
 4.
-SELECT DISTINCT FILMS.price_for_rent FROM ORDERS,FILMS
-WHERE Rental_days <= 3;
+select F.price_for_rent,F.film_name, O.Rental_days,C.Firstname
+from Orders as O
+         join Customers as C
+              on C.Id = O.Customer_id
+         join Films as F
+              on F.Id = O.Film_id
+where rental_days >=3
 5.
-
-6.
+select F.price_for_rent * O.Rental_days as TotalSum,F.film_name, O.Rental_days,C.Firstname,C.Lastname
+from Orders as O
+         join Customers as C
+              on C.Id = O.Customer_id
+         join Films as F
+              on F.Id = O.Film_id
+where rental_days >=3
+order by rental_days desc
+    6.
 SELECT FILM_NAME FROM FILMS
 WHERE AGE_CONSTRAINT<18;
 7.
+select C.Firstname ||  ' '  || C.Lastname as Client,count(c.firstname)
+from Customers as C
+         join Orders as O
+              on C.id = o.customer_id
+group by c.firstname,c.lastname
+8.
+select F.film_name, length(film_name) as length
+from Films as F
+order by film_name desc
+    limit 2
+9.
+select film_name, price_for_rent, count(film_name)*price_for_rent as Totalsum
+from Films as F
+         join Orders as O
+              on F.id = O.Film_id
+group by film_name,price_for_rent
 
 
