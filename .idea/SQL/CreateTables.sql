4a49b808105aab9074191cb59fe19cd421624ea7
walerikq <walerikq@yandex.ru>
2022-12-16 13:50:34 +0300

    fix syntax

--- .idea/SQL/CreateTables.sql
+++ .idea/SQL/CreateTables.sql
@@ -1,29 +1,51 @@
-CREATE TABLE CITY(
-Id serial PRIMARY KEY,
-Name CHARACTER(30)
+-- Напиши скрипт создания таблицы со следующими параметрами:
+-- •	название таблицы - CITY
+-- •	содержит в себе 2 столбца - id, name,
+-- •	первичным ключом будет поле id, который инкрементируется каждый раз при создании города
+create table city(
+id serial primary key,
+name character(30)
 );
 
-CREATE TABLE FILMS(
-Id serial PRIMARY KEY,
-film_name CHARACTER(30) NOT NULL,
-price_for_rent INTEGER CHECK(price_for_rent>0) ,
-age_constraint INTEGER CHECK(age_constraint>0));
+-- Напиши скрипт создания таблицы со следующими параметрами:
+-- •	Название таблицы - FILMS
+-- •	содержит в себе 4 столбца id, film_name, price_for_rent, age_constraint (ограничение по возрасту для выдачи, тип число = возрасту с которого доступен фильм)
+-- •	ограничение > 0 на price и на age, ограничение not null на названия
+-- •	Первичным ключом будет поле id, который инкрементируется каждый раз при создании фильма
+create table films(
+id serial primary key,
+film_name character(30) not null,
+price_for_rent integer check(price_for_rent>0) ,
+age_constraint integer check(age_constraint>0));
 
-CREATE TABLE CUSTOMERS(
-Id serial PRIMARY KEY,
-Firstname CHARACTER(30) NOT NULL,
-Lastname CHARACTER(30) NOT NULL,
-Age INTEGER,
-City_id INTEGER,
-FOREIGN KEY (City_id) REFERENCES CITY(id)
+-- Напиши скрипт создания таблицы со следующими параметрами:
+-- •	название таблицы - CUSTOMERS
+-- •	содержит в себе 5 столбцов - id, first_name, last_name, age, city_id
+-- •	первичным ключом будет поле id, который инкрементируется каждый раз, при создании пользователя
+-- •	ограничение нот налл на поля имени и фамилии
+-- •	внешним ключом на поле id таблицы городов будет city_id
+
+create table customers(
+id serial primary key,
+firstname character(30) not null,
+lastname character(30) not null,
+age integer,
+city_id integer,
+foreign key (city_id) references city(id)
 );
 
-CREATE TABLE ORDERS(
-Id serial PRIMARY KEY,
-Customer_id INTEGER,
-Film_id INTEGER,
-Rental_days INTEGER,	
-FOREIGN KEY (Film_id) REFERENCES FILMS(id),
-FOREIGN KEY (Customer_id) REFERENCES CUSTOMERS(id)
+-- Напиши скрипт создания таблицы со следующими параметрами:
+-- •	название таблицы - ORDERS
+-- •	содержит в себе 4 столбца - id, customer_id, film_id, rental_days(количество дней на которое был взят фильм, тип целое число)
+-- •	первичным ключом будет поле id, который инкрементируется каждый раз, при создании заказа
+-- •	внешним ключом на поле id таблицы пользователей будет customer_id
+-- •	внешним ключом на поле id таблицы фильмов будет film_id
+create table orders(
+id serial primary key,
+customer_id integer,
+film_id integer,
+rental_days integer,
+foreign key (film_id) references films(id),
+foreign key (customer_id) references customers(id)
 );
 
--- .idea/SQL/RequestSQL.sql
+++ .idea/SQL/RequestSQL.sql
@@ -1,47 +1,55 @@
-1 .
-SELECT DISTINCT lastname FROM customers;
-2.
-SELECT lastname FROM customers
-WHERE Firstname LIKE 'А%';
-3.
-SELECT firstname,lastname,CITY.name AS city FROM customers,city
-WHERE CITY.id = customers.id AND NAME LIKE 'Н%';
-4.
-select F.price_for_rent,F.film_name, O.Rental_days,C.Firstname
-from Orders as O
-         join Customers as C
-              on C.Id = O.Customer_id
-         join Films as F
-              on F.Id = O.Film_id
-where rental_days >=3
-5.
-select F.price_for_rent * O.Rental_days as TotalSum,F.film_name, O.Rental_days,C.Firstname,C.Lastname
-from Orders as O
-         join Customers as C
-              on C.Id = O.Customer_id
-         join Films as F
-              on F.Id = O.Film_id
-where rental_days >=3
+--1.Выведи уникальные фамилии заказчиков из таблицы заказчиков
+select distinct lastname from customers;
+
+--2.Доработай запрос так, чтобы выводились фамилии только тех заказчиков, имя которых начинается на русскую "А"
+select lastname from customers
+ where firstname like 'а%';
+
+--3. Выведи имя, фамилию, и город пользователя только для тех заказчиков, город которых начинается с русской буквы Н
+select firstname,lastname,city.name as city from customers,city
+ where city.id = customers.id and name like 'н%';
+
+--4.Получи из таблицы заказов информацию по платежам, у которых количество дней проката больше или равно трем.
+select f.price_for_rent,f.film_name, o.rental_days,c.firstname
+  from orders as o
+  join customers as c
+    on c.id = o.customer_id
+  join films as f
+    on f.id = o.film_id
+ where rental_days >= 3
+
+--5. Доработай запрос так, чтобы вывести для этих заказов еще название фильма, фамилию заказчика и итоговую сумму платежа (rental_days * flims.price_for_rent). (тут тебе пригодится join)
+--   Данные должны быть отсортированы по rental_days в обратном порядке (сначала заказы с самым большим количеством дней) (тут тебе нужен оператор order by)
+select f.price_for_rent * o.rental_days as totalsum,f.film_name, o.rental_days,c.firstname,c.lastname
+  from orders o
+  join customers c
+    on c.id = o.customer_id
+  join films f
+    on f.id = o.film_id
+ where rental_days >= 3
 order by rental_days desc
-    6.
-SELECT FILM_NAME FROM FILMS
-WHERE AGE_CONSTRAINT<18;
-7.
-select C.Firstname ||  ' '  || C.Lastname as Client,count(c.firstname)
-from Customers as C
-         join Orders as O
-              on C.id = o.customer_id
+
+--6. Выведи те film_name которые доступны покупателю с возрастом менее 18 лет
+select film_name from films
+ where age_constraint < 18;
+--7. Выведи одним запросом полное имя заказчика (фамилия+имя в одной колонке) и количество заказов этого человека (для объединения пригодится concat, для количества - group by по идентификатору заказчика и оператор count)
+select c.firstname ||  ' '  || c.lastname as client,count(c.firstname)
+  from customers c
+  join orders o
+    on c.id = o.customer_id
 group by c.firstname,c.lastname
-8.
-select F.film_name, length(film_name) as length
-from Films as F
+
+--8. Выведи одним запросом информацию о двух фильмах с самыми длинными названиями (для вычисления длины пригодится оператор lenght, для ограничения вывода сортировка order by и оператор limit для ограничения результата)
+select f.film_name, length(film_name) as length
+  from films f
 order by film_name desc
     limit 2
-9.
-select film_name, price_for_rent, count(film_name)*price_for_rent as Totalsum
-from Films as F
-         join Orders as O
-              on F.id = O.Film_id
+
+--9. Выведи одним запросом название фильма, цену за аренду, общее количество случаев, когда этот фильм брали в аренду и общую сумму которую выручили за аренду этих фильмов (количество * на цену)
+select film_name, price_for_rent, count(film_name)*price_for_rent as totalsum
+  from films f
+  join orders o
+    on f.id = o.film_id
 group by film_name,price_for_rent
 
 
